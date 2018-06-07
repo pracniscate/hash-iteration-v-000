d41af5a711bce57c238317a234cb29115925b37f
Lorena <lorena.giddens@outlook.com>
2018-06-07 14:00:52 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,14 @@
 
 def select_winner(passengers)
   winner = ""
+  # iterate through the hash using #each
   passengers.each do |suite, name|
+    # check if we have the right suite and if the person in that suite has a name that begins with letter "A"
     if suite == :suite_a && name.start_with?("A")
+      # if condition is true, we set the winner's name equal to the 'winner' variable and end our iteration
       winner = name
     end
   end
+  # call on the 'winner' variable to return the name of the lucky winner
   winner
 end
